a8b539ef3ee72948a6e6b7018abb38b609026cf2
alexbonti <aledeakin@gmail.com>
2018-11-12 22:37:30 +1100

    fixed the test error

--- src/views/main/main.jsx
+++ src/views/main/main.jsx
@@ -4,17 +4,21 @@ import Home from 'views/home/home.jsx'
 import Team from 'views/team/team.jsx'
 import Login from 'views/login/login.jsx'
 import { Switch, Route } from 'react-router-dom'
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
-       <Switch>
+      <BrowserRouter>
+      <Switch>
             <Route exact path='/' component={Login}/>
             <Route exact path='/team' component={Team}/>
             <Route exact path='/home' component={Home}/>
         </Switch>
+      </BrowserRouter>
+       
       </div>
     );
   }
