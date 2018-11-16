980487a3f308fbb20c84d58eb22ba5f9f1cfefbb
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-16 12:05:29 +1100

    appJS: Clean

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react'
-import logo from './images/deakin-logo.svg'
-import simpleHelper from './helpers/simpleHelper'
-import Main from './views/main/main.jsx'
-import './App.css'
-import 'materialize-css'; // It installs the JS asset only
+import Main from 'views/main/main.jsx'
+import 'App.css'
 import 'materialize-css/dist/css/materialize.min.css'
 import 'material-icons/iconfont/material-icons.css'
 
-let randomN=new simpleHelper()
-console.log(randomN.generateRandomNumber())
-console.log(randomN.generateRandomInt(10,1000))
-/*fetch('/api/hello').then((response)=>response.json()).then((responseJson)=>{
-  console.log('done')
-})*/
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn:false ,
+      isLoggedIn: false,
     };
   }
   render() {    
     return (
       <div className="App">
-       
         <Main />
       </div>
     );
