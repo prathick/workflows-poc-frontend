d56f8108e7ddf48d3aedf53148ca7cb1bd6f51ef
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-16 11:48:06 +1100

    main: Remove BrowserRouter since we have it in index.js

--- src/views/main/main.jsx
+++ src/views/main/main.jsx
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
-import logo from 'images/deakin-logo.svg'
 import Home from 'views/home/home.jsx'
 import Team from 'views/team/team.jsx'
 import Login from 'views/login/login.jsx'
 import { Switch, Route } from 'react-router-dom'
-import BrowserRouter from 'react-router-dom/BrowserRouter';
 
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
-      <BrowserRouter>
       <Switch>
             <Route exact path='/' component={Login}/>
             <Route exact path='/team' component={Team}/>
             <Route exact path='/home' component={Home}/>
         </Switch>
-      </BrowserRouter>
-       
       </div>
     );
   }
