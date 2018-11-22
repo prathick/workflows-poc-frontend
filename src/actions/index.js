79a1ddeda6efd2cd843d79aadeced8866a4092b8
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-22 14:44:09 +1100

    login: Add login, logout, accessTokenLogin actions for Redux

---
+++ src/actions/index.js
@@ -0,0 +1,35 @@
+export const REQUEST_LOGIN = "REQUEST_LOGIN"
+export const REQUEST_LOGOUT = "REQUEST_LOGOUT"
+export const REQUEST_ACCESS_TOKEN_LOGIN = "REQUEST_ACCESS_TOKEN_LOGIN"
+export const SET_USER_ROLE = "SET_USER_ROLE"
+
+export const requestLogin = (data) => ({
+  type: REQUEST_LOGIN,
+  payload: {
+    request: {
+      url: 'user/login',
+      method: 'POST',
+      data: {
+        emailId: data.emailId,
+        password: data.password
+      }
+    }
+  }
+})
+
+export const requestAccessTokenLogin = (token) => ({
+  type: REQUEST_ACCESS_TOKEN_LOGIN,
+  payload: {
+    request: {
+      url: 'user/accessTokenLogin',
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+  }
+})
+
+export const requestLogout = () => ({
+  type: REQUEST_LOGOUT
+})
