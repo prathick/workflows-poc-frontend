511f824aa5ab40c30c184bb0155df6243e83a5ee
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-22 14:33:26 +1100

    login: Add reducers for login/logout Redux

---
+++ src/reducers/login.js
@@ -0,0 +1,42 @@
+import { REQUEST_LOGIN, REQUEST_LOGOUT, REQUEST_ACCESS_TOKEN_LOGIN } from "actions";
+const REQUEST_LOGIN_SUCCESS = "REQUEST_LOGIN_SUCCESS"
+const REQUEST_LOGIN_FAIL = "REQUEST_LOGIN_FAIL"
+
+const REQUEST_LOGOUT_SUCCESS = "REQUEST_LOGOUT_SUCCESS"
+const REQUEST_LOGOUT_FAIL = "REQUEST_LOGOUT_FAIL"
+
+const REQUEST_ACCESS_TOKEN_LOGIN_SUCCESS = "REQUEST_ACCESS_TOKEN_LOGIN_SUCCESS"
+const REQUEST_ACCESS_TOKEN_LOGIN_FAIL = "REQUEST_ACCESS_TOKEN_LOGIN_FAIL"
+
+const login = (state = {loading: false, loggedIn: false, userRole: ''}, action) => {
+  switch(action.type) {
+    // Login and accessTokenLogin
+    case REQUEST_LOGIN:
+    case REQUEST_ACCESS_TOKEN_LOGIN:
+      return {...state, loading: true, loggedIn: false}
+
+    case REQUEST_LOGIN_SUCCESS:
+    case REQUEST_ACCESS_TOKEN_LOGIN_SUCCESS:
+      return {...state, loading: false, loggedIn: true}
+
+    case REQUEST_LOGIN_FAIL:
+    case REQUEST_ACCESS_TOKEN_LOGIN_FAIL:
+      return {...state, loading: false, loggedIn: false}
+
+    // Logout
+    case REQUEST_LOGOUT:
+      return {...state, loading: false, loggedIn: false, userRole: ''}
+
+    case REQUEST_LOGOUT_SUCCESS:
+      return {...state, loading: false, loggedIn: false}
+
+    case REQUEST_LOGOUT_FAIL:
+      return {...state, loading: false, loggedIn: true}
+
+
+    default:
+      return {loading: false, loggedIn: false, userRole: ''}
+  }
+}
+
+export default login;
