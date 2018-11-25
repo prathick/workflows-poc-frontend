7f838879c69c3018665bbfd59af28f57365640d0
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-26 00:10:50 +1100

    mainComponent: Remove login route

--- src/views/main/main.jsx
+++ src/views/main/main.jsx
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Home from 'views/home/home.jsx'
 import Team from 'views/team/team.jsx'
-import Login from 'views/login/login.jsx'
 import { Switch, Route } from 'react-router-dom'
-
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
       <Switch>
-          <Route exact path='/' component={Login}/>
-          <Route exact path='/team' component={Team}/>
-          <Route exact path='/home' component={Home}/>
-        </Switch>
+        <Route exact path='/team' component={Team}/>
+        <Route exact path='/home' component={Home}/>
+      </Switch>
       </div>
     );
   }
