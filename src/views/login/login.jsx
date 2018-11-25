48c028d6646c0057bae7704f7147fb7ded4c8e49
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-25 23:39:40 +1100

    loginComponent: Add redux login support

--- src/views/login/login.jsx
+++ src/views/login/login.jsx
@@ -1,23 +1,64 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import AppHelper from "helpers/AppHelper.js";
+import { connect } from 'react-redux';
+import { requestLogin } from 'actions';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailId: '',
+      password: '',
+      isLoggedIn: false,
+    };
+  }
+
+  handleEmailChange=(e)=> {
+    this.setState({
+      emailId: e.target.value
+    });
+  }
+
+  handlePasswordChange=(e)=> {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  performLogin = () => {
+    this.props.dispatchLogin(this.state).then((response) => {
+      const accessToken = response.payload.data.data.accessToken;
+      AppHelper.loginUser(true, accessToken);
+    });
+    // API.loginUser(this.state,this.props.parentProps.parentStateHandler)
+  }
+
   render() {
     return (
       <div className="Login">
         <h1>
-          Login
+          {this.props.parentState.title}
         </h1>
         <div className='row'>
-          <Link to='/home'><a class="waves-effect waves-light btn">
-      
-          <i class="material-icons left">cloud</i>Login
-        </a></Link>
-
+          <div className='row'>
+            <div className='col s6 offset-s3'>
+              <input placeholder="Email" id="email" type="email" className="validate" onChange={this.handleEmailChange} />
+              <input placeholder="Password" id="password" type="password" className="validate" onChange={this.handlePasswordChange} />
+              <a className="waves-effect waves-light btn" onClick={this.performLogin} href="#!">
+                <i className="material-icons left">cloud</i>Login
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatchLogin  : (data) => dispatch(requestLogin(data))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Login);
