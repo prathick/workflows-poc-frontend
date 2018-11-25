499da307e76a939212a5537a0a3736aaede451a5
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-25 23:20:47 +1100

    footerComponent: New

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render () {
+    return (
+      <div>Modify components/footer.jsx</div>
+    )
+  }
+}
+
+export default Footer;
