3f12bbce33ce0af0d9ac05d2f0bb3b008c459296
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-25 23:37:22 +1100

    indexJS: Add redux support, export axiosClient, minor changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import BrowserRouter  from 'helpers/router';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
+import rootReducer from 'reducers';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-    
-  , document.getElementById('root'));
+export const baseURL = process.env.REACT_APP_BASE_URL + "/api";
+
+export const axiosClient = axios.create({
+  baseURL: baseURL,
+  responseType: 'json'
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(axiosMiddleware(axiosClient))
+);
+const store = createStore(rootReducer, enhancer);
+
+render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
