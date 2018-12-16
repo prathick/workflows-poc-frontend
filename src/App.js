d2cda3df7861793269fcc3eb4d5205ce90014b17
Akash Agarwal <akashaggarwal777@gmail.com>
2018-12-17 10:38:36 +1100

    appJS: Add ability to visit external resource without being logged in
    It's not the perfect way since main.jsx is not utilised now
    Every protected route needs to have an additional check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import Main from 'views/main/main.jsx'
 import './App.scss'
 import 'materialize-css/dist/css/materialize.min.css'
 import 'material-icons/iconfont/material-icons.css'
@@ -8,9 +7,11 @@ import AppHelper from "helpers/AppHelper.js";
 import Footer from 'components/footer.jsx';
 import LoadingComponent from 'components/loading/loading.jsx';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { requestAccessTokenLogin } from 'actions';
 import Login from 'views/login/login.jsx';
+import Home from 'views/home/home.jsx';
+import Team from 'views/team/team.jsx';
 
 class App extends Component {
   constructor(props) {
@@ -32,20 +33,43 @@ class App extends Component {
     if ((token = AppHelper.isUserLocalStorageLoggedIn())) {
       if (token === 'true') return;
       this.props.dispatchAccessTokenLogin(token)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch ((error) => console.log(error));
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => console.log(error));
     }
   }
 
   render() {
-    if (this.props.loading) return (<LoadingComponent/>);
+    if (this.props.loading) return (<LoadingComponent />);
     else return (
       <div className="App">
-        {this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? <Header title={this.state.title} logout={this.stateHandler}/> : ''}
-        {this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? <Main parentState={this.state} parentStateHandler={this.stateHandler} parentProps={this.props}/> : <Login parentState={this.state} parentProps={this.props}/>}
-        {this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? <Footer/> : ''}
+        {/* Header */}
+        {this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? <Header title={this.state.title} logout={this.stateHandler} /> : ''}
+        
+        {/* Main body */}
+        <Switch>
+          
+          <Route exact path='/' render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <Redirect to='/home' /> : <Login parentState={this.state} parentProps={this.props} /> )}
+          />
+
+          <Route exact path='/team' render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
+            <Team {...props}/> : <Redirect to='/' /> )} 
+          />
+
+          <Route exact path='/home' render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
+            <Home {...props}/> : <Redirect to='/' /> )} 
+          />
+
+          <Route exact path='/test' render={() => <div>Test</div>} />
+
+          <Route render={() => <div>404 Error</div>} />
+
+        </Switch>
+
+        {/* Footer */}
+        {this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? <Footer /> : ''}
       </div>
     );
   }
@@ -53,14 +77,14 @@ class App extends Component {
 
 const mapStateToProps = (state) => {
   return {
-      loggedIn : state.loginStatus.loggedIn,
-      loading : state.loginStatus.loading
+    loggedIn: state.loginStatus.loggedIn,
+    loading: state.loginStatus.loading
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    dispatchAccessTokenLogin : (token) => dispatch(requestAccessTokenLogin(token))
+    dispatchAccessTokenLogin: (token) => dispatch(requestAccessTokenLogin(token))
   }
 }
 
